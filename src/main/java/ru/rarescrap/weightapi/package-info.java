25b1a93732bd98b6a5cb30bc8c4143e97e5b6230
RareScrap <rarescrap@users.noreply.github.com>
2019-04-10 15:29:39 +0800

    feat(core): API оформлен в принятом Forge-стиле
    + Заполнен mcmod.info

---
+++ src/main/java/ru/rarescrap/weightapi/package-info.java
@@ -0,0 +1,4 @@
+@API(owner = APIContainer.MODID, provides = "WeightAPI", apiVersion = APIContainer.VERSION) // TODO: Можно ли поместить эту аннотацию в APIContainer?
+package ru.rarescrap.weightapi;
+
+import cpw.mods.fml.common.API;
