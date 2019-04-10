28321242a67ebaba5c1ac44febbfd4e13abd996f
RareScrap <rarescrap@users.noreply.github.com>
2019-04-10 15:27:48 +0800

    refactor(core): Изменено имя главного класса

---
+++ src/main/java/ru/rarescrap/weightapi/APIContainer.java
@@ -0,0 +1,9 @@
+package ru.rarescrap.weightapi;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = APIContainer.MODID, version = APIContainer.VERSION)
+public class APIContainer  {
+    public static final String MODID = "weightapi";
+    public static final String VERSION = "0.2.0";
+}
