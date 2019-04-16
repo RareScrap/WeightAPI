36b2e71cfab1501f28146822917dd5539742611c
RareScrap <rarescrap@users.noreply.github.com>
2019-04-16 19:14:45 +0800

    refactor(core): Удален IWeightTracker в пользу евентов
    Если вдруг сложится ситуация, когда нужно расчитывать вес инвентаря вне зависимости от текущего IWeghtProvider'а, евенты дадут больше гибкости.

--- src/main/java/ru/rarescrap/weightapi/WeightRegistry.java
+++ src/main/java/ru/rarescrap/weightapi/WeightRegistry.java
@@ -1,15 +1,8 @@
 package ru.rarescrap.weightapi;
 
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class WeightRegistry {
 
     private static IWeightProvider weightProvider;
-    private static Map<IInventory, IWeightTracker> trackers = new HashMap<IInventory, IWeightTracker>();
 
     // Трекеры нужно регистрировать по большому счету только для того, чтобы иметь возможность получить их из других мест
     public static void register(IWeightTracker tracker) {
@@ -28,18 +21,5 @@ public class WeightRegistry {
 
     public static IWeightProvider getWeightProvider() {
         return weightProvider;
-    }
-
-    // TODO: Опасно пиздец. Стоит ли оставлять?
-    public static IWeightTracker findTracker(ItemStack itemStack) {
-        for (Map.Entry<IInventory, IWeightTracker> entry : trackers.entrySet()) {
-            IInventory inventory = entry.getKey();
-            for (int i = 0; i < inventory.getSizeInventory(); i++) {
-                ItemStack itemStack1 = inventory.getStackInSlot(i);
-                if (itemStack1 == itemStack) return trackers.get(inventory);
-            }
-        }
-
-        return null;
     }
 }
