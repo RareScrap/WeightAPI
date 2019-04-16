2a990209d47b2d7ac09e77e1e4f4bcc0f70edeb2
RareScrap <rarescrap@users.noreply.github.com>
2019-04-16 19:18:51 +0800

    feat(core): Теперь при попытке зарегистрировать провайдер будет краш, если другой провайдер уже был зарегистрирован.
    Я думаю, что ситуация когда в коде есть несколько провайдеров - бессмысленна. Зачем тогда это поддерживать?

--- src/main/java/ru/rarescrap/weightapi/APIContainer.java
+++ src/main/java/ru/rarescrap/weightapi/APIContainer.java
@@ -1,9 +1,17 @@
 package ru.rarescrap.weightapi;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLServerStoppedEvent;
 
 @Mod(modid = APIContainer.MODID, version = APIContainer.VERSION)
 public class APIContainer  {
     public static final String MODID = "weightapi";
     public static final String VERSION = "0.2.0";
+
+    @Mod.EventHandler
+    public void onStop(FMLServerStoppedEvent event) {
+        // т.к. при завершении мира в сингле провайдер все еще
+        // сохраняется и вызовет краш при следующем заходе в игру
+        WeightRegistry.clearProvider();
+    }
 }
--- src/main/java/ru/rarescrap/weightapi/WeightRegistry.java
+++ src/main/java/ru/rarescrap/weightapi/WeightRegistry.java
@@ -11,12 +11,17 @@ public class WeightRegistry {
     }
 
     public static void registerWeightProvider(IWeightProvider provider) {
-        weightProvider = provider;
-        // TODO: Event?
+        if (weightProvider == null) weightProvider = provider;
+        /* Не вижу смысла регистрировать новый провайдер если в системе уже есть зареганый.
+         * Тогда придется парить с реализацией замеяемости, что не нужно, имхо. И какая вообще
+         * логика в установке двух систем веса? */
+        else throw new RuntimeException("Attempt to change registered weight provider: "
+                + weightProvider.getClass().getName() + " to " + provider.getClass().getName());
     }
 
-    public static IWeightTracker getTracker(IInventory inventory) {
-        return trackers.get(inventory);
+    // не для использования за пределами апи
+    static void clearProvider() {
+        weightProvider = null;
     }
 
     public static IWeightProvider getWeightProvider() {
